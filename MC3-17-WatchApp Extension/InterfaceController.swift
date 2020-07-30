0764a601a90aeb8f757c2c93941b1fada0794fcb
Sherwin Yang <sherwinyang@Sherwins-MacBook-Pro.local>
2020-07-30 19:58:03 +0700

    WatchApp extention added

---
+++ MC3-17-WatchApp Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MC3-17-WatchApp Extension
+//
+//  Created by Sherwin Yang on 30/07/20.
+//  Copyright © 2020 Sherwin Yang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
